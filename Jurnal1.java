b32bc30a1f78e6d1adea18a96405e1c0ee7c1f98
Muhammad Zhafran <mzhafraan@gmail.com>
2024-09-26 12:47:35 +0700

    Edit file Jurnal1

--- Jurnal1.java
+++ Jurnal1.java
@@ -1,7 +1,6 @@
 public class Jurnal1 {
 
     public static void main(final String[] args) {
-        System.out.println("NIM : 607062430006");
-        System.out.println("Nama : Muhammad Zhafran Mufadhdhol");
-    }
+        System.out.println("Hello World !!");
+            }
 }
